be295d349a1f5cca3c545bbd3b62e3c4a20bb46e
Miles <milesredgate@gmail.com>
2018-10-02 15:52:38 -0300

    second commit

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-<footer class="footer">
-	<div class="container-fluid">
-	</div>
-</footer>
-<script src="text/javascript" src="js/jquery.js"></script>
-<script src="text/javascript" src="js/bootstrap.bundle.js"></script>
-</body>
-</html>
